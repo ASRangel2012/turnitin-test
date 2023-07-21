17693d88ef35054e4ab3650a39a167580e8ab27a
Angel <ASRAngel2012@outlook.com>
2023-07-21 13:22:36 -0400

    Organized code, fixed email case sensitivity problem by converting the search,username and email to lowercase. In addition, addressed (X) button on user details modal

--- php-backend/src/web/api.php
+++ php-backend/src/web/api.php
@@ -1,17 +1,27 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json'); //Set the response content type to JSON
+
+//Split the request path by slashes
 $pathInfo = explode('/', $_SERVER['PATH_INFO']);
+
+//Check if the path is valid (contains at least two elements)
 if (count($pathInfo) < 2) {
     echo json_encode([
         'error' => 'Invalid path'
     ]);
     exit;
 }
+
+//Connect to the PostgreSQL database
 $dbconn = pg_connect("host=db dbname=postgres user=postgres password=postgres");
 
+//Get the resource from the path
 $resource = $pathInfo[1];
+
+//Handle different resources based on the request
 switch ($resource) {
     case 'members':
+        //If the resource is 'members', fetch all memberships from the database
         $result = pg_query_params($dbconn, 'SELECT * FROM memberships', []);
         $memberships = [];
         while($membership = pg_fetch_assoc($result)) {
@@ -22,12 +32,15 @@ switch ($resource) {
         ]);
         break;
     case 'users':
+        //If the resource is 'users', check if a specific user ID is provided in the path
         if (isset($pathInfo[2])) {
             $userId = $pathInfo[2];
+            //Fetch the user with the provided ID from the database
             $result = pg_query_params($dbconn, 'SELECT * FROM users WHERE id = $1', [$userId]);
             $user = pg_fetch_assoc($result);
             echo json_encode($user);
         } else {
+            //If no specific user ID is provided, fetch all users from the database
             $result = pg_query_params($dbconn, 'SELECT * FROM users', []);
             $users = [];
             while($user = pg_fetch_assoc($result)) {
@@ -39,6 +52,7 @@ switch ($resource) {
         }
         break;
     default:
+        // If the resource is not 'members' or 'users', return an error
         echo json_encode([
             'error' => 'Invalid resource'
         ]);
