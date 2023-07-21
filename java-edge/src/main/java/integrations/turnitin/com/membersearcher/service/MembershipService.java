7153d851f86d0079191ada1399d8ca66f4e89143
Angel <ASRAngel2012@outlook.com>
2023-07-21 13:02:48 -0400

    Made MembershipService more modular, easier to read now, added null checks, and fixed all tests.

--- java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
+++ java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
@@ -2,52 +2,75 @@ package integrations.turnitin.com.membersearcher.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
 import integrations.turnitin.com.membersearcher.model.MembershipList;
 import integrations.turnitin.com.membersearcher.model.Membership;
 import integrations.turnitin.com.membersearcher.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import integrations.turnitin.com.membersearcher.model.UserList;
 
+/**
+ * Service class to fetch memberships with associated user details.
+ */
 @Service
 public class MembershipService {
+
     @Autowired
     private MembershipBackendClient membershipBackendClient;
 
     /**
-     * Fetch all memberships with associated user details 
+     * Fetch all memberships with associated user details.
+     *
      * @return A CompletableFuture containing a fully populated MembershipList object.
      */
-	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
-    	return membershipBackendClient.fetchMemberships()
-            .thenCompose(members -> {
-                //Collect all the user IDs from memberships into a list
-                List<String> listOfUserIds = members.getMemberships().stream()
-                        .map(Membership::getUserId)
-                        .collect(Collectors.toList());
+    public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
+        return membershipBackendClient.fetchMemberships()
+                .thenCompose(members -> {
+                    if (members.getMemberships().isEmpty()) {
+                        //Return an empty MembershipList if there are no memberships
+                        return CompletableFuture.completedFuture(new MembershipList());
+                    } else {
+                        return fetchUsersForMemberships(members)
+                                .thenApply(users -> associateUsersWithMemberships(members, users));
+                    }
+                });
+    }
+    
+    /**
+     * Fetch users for the given memberships in bulk using their user IDs.
+     *
+     * @param memberships The list of memberships for which to fetch users.
+     * @return A CompletableFuture containing a UserList object containing the fetched users.
+     */
+    private CompletableFuture<UserList> fetchUsersForMemberships(MembershipList memberships) {
+        List<String> listOfUserIds = memberships.getMemberships().stream()
+                .map(Membership::getUserId)
+                .collect(Collectors.toList());
 
-                //Get all users by passing in listOfUserIds to backend client 
-                return membershipBackendClient.fetchUsers(listOfUserIds)
-                        .thenApply(users -> {
-                            //Map each userID with its User 
-                            Map<String, User> userMap = users.getUsers().stream()
-                                    .collect(Collectors.toMap(
-										User::getId,
-									 	user -> {
-											//just return the user object. note: user can be modified here, if needed. 
-											return user;
-										}
-									)); 
-                            // Associate users with their corresponding memberships 
-                            members.getMemberships().forEach(member -> {
-                                User user = userMap.get(member.getUserId());
-                                member.setUser(user);
-                            });
+        return membershipBackendClient.fetchUsers(listOfUserIds);
+    }
 
-                            return members;
-                        });
-            });
-	}
+     /**
+     * Associate fetched users with their corresponding memberships.
+     *
+     * @param userList The UserList containing the fetched users.
+     * @return The MembershipList with associated users.
+     */
+    private MembershipList associateUsersWithMemberships(MembershipList memberships, UserList userList) {
+        Map<String, User> userMap = userList.getUsers().stream()
+                .collect(Collectors.toMap(User::getId, user -> user));
+    
+        memberships.getMemberships().forEach(member -> {
+            User user = userMap.get(member.getUserId());
+            // Set the user only if it exists, otherwise set it to null 
+            member.setUser(user != null ? user : null);
+        });
+        return memberships;
+    }
 }
+
